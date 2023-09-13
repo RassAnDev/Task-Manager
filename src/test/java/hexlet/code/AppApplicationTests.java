ece6989a8d81976fbca5219fb5bddcf66d12bca8
Andrew <rassandev@gmail.com>
2023-09-13 14:37:55 +0300

    fix AppApplicationTests.java class

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -1,11 +1,8 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 class AppApplicationTests {
 
     @Test
