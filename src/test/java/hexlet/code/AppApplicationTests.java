5d5535d672a4fc285b70ee135c94c105278730ad
Andrew <rassandev@gmail.com>
2023-09-13 09:07:16 +0300

    fix build.gradle

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -3,11 +3,14 @@ package hexlet.code.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class AppApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testInit() {
+        assertThat(true).isTrue();
     }
 
 }
