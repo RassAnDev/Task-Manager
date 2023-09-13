59315037b1af4c46a14d282fe778754f9488af3b
Andrew <rassandev@gmail.com>
2023-09-13 09:11:15 +0300

    fix User.java class

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -2,17 +2,22 @@ package hexlet.code.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
 
@@ -29,16 +34,28 @@ public class User {
     private Long id;
 
     @NotBlank
+    @Column(unique = true)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 1, message = "Имя должно быть не менее 1 символа")
     private String firstName;
 
     @NotBlank
+    @Size(min = 1, message = "Фамилия должна быть не менее 1 символа")
     private String lastName;
 
-    private String email;
-
     @JsonIgnore
+    @NotBlank
     @Size(min = 3, max = 50, message = "Пароль должен быть от 3 до 50 символов")
     private String password;
 
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
+
+    public User(final Long id) {
+        this.id = id;
+    }
 }
