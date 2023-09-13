8b851338c4797fe8583bc66c9003779920af280b
Andrew <rassandev@gmail.com>
2023-09-13 09:11:59 +0300

    fix UserRepository.java class

--- src/main/java/hexlet/code/repository/UserRepository.java
+++ src/main/java/hexlet/code/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package hexlet.code.repository;
 
 import hexlet.code.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
 
