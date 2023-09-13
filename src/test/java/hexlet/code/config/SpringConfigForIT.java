b2cefd6ec75a50ff68921e3823616f7bbe7316e4
Andrew <rassandev@gmail.com>
2023-09-13 14:39:19 +0300

    fix Spring configuration for tests

--- src/test/java/hexlet/code/config/SpringConfigForIT.java
+++ src/test/java/hexlet/code/config/SpringConfigForIT.java
@@ -15,7 +15,7 @@ import static hexlet.code.config.SpringConfigForIT.TEST_PROFILE;
 @Profile(TEST_PROFILE)
 @EnableAspectJAutoProxy
 @ComponentScan(basePackages = "hexlet.code")
-@PropertySource(value = "classpath:/config/application.yaml")
+@PropertySource(value = "classpath:/config/application.yml")
 public class SpringConfigForIT {
 
     public static final String TEST_PROFILE = "test";
