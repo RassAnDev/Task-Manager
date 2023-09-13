0713f9d10e5c7fa6c18b82d6b2c663177a87e6bf
Andrew <rassandev@gmail.com>
2023-09-13 09:24:56 +0300

    add utils class for tests

---
+++ src/test/java/hexlet/code/utils/TestUtils.java
@@ -0,0 +1,90 @@
+package hexlet.code.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.component.JWTHelper;
+import hexlet.code.dto.UserDto;
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.Map;
+
+import static hexlet.code.controller.UserController.USER_CONTROLLER_PATH;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@Component
+public class TestUtils {
+
+    public static final String TEST_USERNAME = "kaermorhen@gmail.com";
+    public static final String TEST_USERNAME_2 = "vengerberg@gmail.com";
+
+    private final UserDto testDtoForRegistration = new UserDto(
+            TEST_USERNAME,
+            "Geralt",
+            "from Rivia",
+            "yennefer"
+    );
+
+    public UserDto getTestDtoForRegistration() {
+        return testDtoForRegistration;
+    }
+
+    @Autowired(required = true)
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JWTHelper jwtHelper;
+
+
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    public User getUserByEmail(final String email) {
+        return userRepository.findByEmail(email).get();
+    }
+
+    public ResultActions regDefaultUser() throws Exception {
+        return regUser(testDtoForRegistration);
+    }
+
+    public ResultActions regUser(final UserDto dto) throws Exception {
+        final MockHttpServletRequestBuilder request = post(USER_CONTROLLER_PATH)
+                .content(asJson(dto))
+                .contentType(APPLICATION_JSON);
+
+        return perform(request);
+    }
+
+    public ResultActions perform(final MockHttpServletRequestBuilder request, final String byUser) throws Exception {
+        final String token = jwtHelper.expiring(Map.of("username", byUser));
+        request.header(AUTHORIZATION, token);
+
+        return perform(request);
+    }
+
+    public ResultActions perform(final MockHttpServletRequestBuilder request) throws Exception {
+        return mockMvc.perform(request);
+    }
+
+    private static final ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules();
+
+    public static String asJson(final Object object) throws JsonProcessingException {
+        return MAPPER.writeValueAsString(object);
+    }
+
+    public static <T> T fromJson(final String json, final TypeReference<T> to) throws JsonProcessingException {
+        return MAPPER.readValue(json, to);
+    }
+}
