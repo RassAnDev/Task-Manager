e88cd91cac9e22326e48f7c6a4819d994a6c9c40
Andrew <rassandev@gmail.com>
2023-09-21 13:53:59 +0300

    fix TaskDto.java

--- src/main/java/hexlet/code/dto/TaskDto.java
+++ src/main/java/hexlet/code/dto/TaskDto.java
@@ -1,7 +1,5 @@
 package hexlet.code.dto;
 
-import hexlet.code.model.TaskStatus;
-import hexlet.code.model.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
