af19923373619f018930a8bb24578fa10832557b
Andrew <rassandev@gmail.com>
2023-09-22 14:11:11 +0300

    add filtration for TaskRepository

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -2,9 +2,10 @@ package hexlet.code.repository;
 
 import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.Optional;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, QuerydslPredicateExecutor<Task> {
     Optional<Task> findByName(String name);
 }
