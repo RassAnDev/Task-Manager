b6896fef26c2994007a550494a023c96f9d651a9
Andrew <rassandev@gmail.com>
2023-09-22 12:13:27 +0300

    add crud repository for Labels

---
+++ src/main/java/hexlet/code/repository/LabelRepository.java
@@ -0,0 +1,10 @@
+package hexlet.code.repository;
+
+import hexlet.code.model.Label;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LabelRepository extends JpaRepository<Label, Long> {
+    Optional<Label> findByName(String name);
+}
